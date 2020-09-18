56862cc32b06d904d6ab701227a947587134a3ae
Mfon Eti-mfon <mfonetimfon@gmail.com>
2020-09-18 23:13:57 +0100

    Rename things better, again

--- tests/test_api.py
+++ tests/test_api.py
@@ -3,10 +3,10 @@ import pytest
 from twapipy import TwAPI
 
 
-@pytest.mark.usefixtures("requests_mock", "mock_post_bearer_token_endpoint")
-def test_api_get_endpoint(
-    api_key, api_key_secret, mock_api_get_endpoint, mock_json_from_api_get_endpoint
-):
+@pytest.mark.usefixtures(
+    "requests_mock", "mock_post_bearer_token_endpoint", "mock_api_get_endpoint"
+)
+def test_api_get_endpoint(api_key, api_key_secret, mock_json_from_api_get_endpoint):
     api = TwAPI(api_key, api_key_secret)
     response = api.get(
         "/2/tweets/search/recent", params={"max_results": "3", "query": "python"}
